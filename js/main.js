e474d443e3a3e255e8178dea9cd1248eba3a1a9c
AlejandroGayoso <alejandro.gayoso@teamsoft.com.pe>
2023-06-26 23:57:09 -0500

    audio

--- js/main.js
+++ js/main.js
@@ -23,3 +23,32 @@ function slides(){
         slider.style.transition = 'transform 1s';
     }
 }
+
+function PlayAudio(){
+    document.getElementById("audio").play();
+}
+
+window.sr = ScrollReveal();
+sr.reveal('.navbar',{
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.header-content-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.header-content-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+        });
+    });
+});
